f5c0395574435f45452081b9108fef33fa498829
Ajayiade <57672853+AdebolaAj@users.noreply.github.com>
2022-10-13 21:46:06 +0000

    Signals lab

--- .ccls-cache/@home@runner@Lab-2-Signal/timer.c
+++ .ccls-cache/@home@runner@Lab-2-Signal/timer.c
@@ -1,47 +1,42 @@
 /* timer.c */
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
 #include <time.h>
-#include <stdbool.h>
+#include <unistd.h>
 
-bool cont = true; 
+bool cont = true;
 int num_of_alarms = 0;
 time_t start, end;
 
-
-void handler(int signum)
-{ //signal handler
+void handler(int signum) { // signal handler
   printf("Hello World!\n");
-    cont = false;
+  cont = false;
   // exit(1); //exit after printing
 }
 
-void handler_interrupt(int signum){
+void handler_interrupt(int signum) {
   end = time(NULL);
   printf("caught SIGINT Signal!\n");
-  printf("Time taken to execute the program: %1f seconds\n",(double)(end-start));
-  printf("Number of alarms that occured: %d\n" ,num_of_alarms);
+  printf("Time taken to execute the program: %1f seconds\n",
+         (double)(end - start));
+  printf("Number of alarms that occured: %d\n", num_of_alarms);
   exit(1);
 }
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
   start = time(NULL);
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  signal(SIGALRM, handler); // register handler to handle SIGALRM
   signal(SIGINT, handler_interrupt);
   while (true) {
-    alarm(1); //Schedule a SIGALRM for 1 second
+    alarm(1); // Schedule a SIGALRM for 1 second
     pause();
-    while(cont); 
+    while (cont)
+      ;
     printf("Turning was right!\n");
     cont = true;
-    num_of_alarms +=1;
-
-
-  
+    num_of_alarms += 1;
   }
-  return 0; //never reached
+  return 0; // never reached
 }
-
---
+++ makefile
@@ -0,0 +1,5 @@
+signals: signal.c
+	gcc signal.c -o signalab 
+
+signals: timer.c 
+	gcc timer.c -o signalab
